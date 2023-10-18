e896ac7401601b15b807c90456887d4796b32f58
Gyeongyeong Kim <74523540+kky5738@users.noreply.github.com>
2023-10-18 20:25:06 +0900

    update

--- App.js
+++ App.js
@@ -1,12 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View , Image} from 'react-native';
+import * as ImagePicker from 'expo-image-picker'
+import { useState } from 'react';
+
+import Button from './components/Butten';
+import ImageViewer from './components/ImageViewr';
 
 const PlaceholderImage = require('./assets/images/background-image.png')
 
 export default function App() {
+  const [selectedImage, setSelectedImage] = useState(null)
+
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    })
+
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri)
+    } else {
+      alert('You did not select any image.')
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Image source={PlaceholderImage} style={styles.image}/>
+      <View style={styles.imageContainer}>
+        <ImageViewer 
+          placeholderImageSource={PlaceholderImage}
+          selectedImage={selectedImage}
+         />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button theme="primary" label="Choose a photo" onPress={pickImageAsync} />
+        <Button label="Use this photo" />
+      </View>
+      <StatusBar style='auto' />
     </View>
   );
 }
@@ -25,5 +55,9 @@ const styles = StyleSheet.create({
     width:320,
     height: 440,
     borderRadius: 18,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
   },
 });
