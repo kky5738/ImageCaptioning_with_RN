1705f91d11d84359b3131f92268c3313225fba3d
Gyeongyeong Kim <74523540+kky5738@users.noreply.github.com>
2023-10-18 17:48:10 +0900

    create project

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View , Image} from 'react-native';
+
+const PlaceholderImage = require('./assets/images/background-image.png')
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image source={PlaceholderImage} style={styles.image}/>
     </View>
   );
 }
@@ -13,8 +14,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#25292e',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
+  },
+  image: {
+    width:320,
+    height: 440,
+    borderRadius: 18,
   },
 });
